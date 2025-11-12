b1d787852693de54af11fc17e27a729b455ce5ac
ShaqayeqMajdi <shaqayeqmajdiazim@gmail.com>
2025-11-12 23:32:18 +0330

    replace useEffect fetch with useGetProducts in admin

--- src/features/admin/AdminPage.jsx
+++ src/features/admin/AdminPage.jsx
@@ -1,39 +1,22 @@
-import { useEffect, useState } from "react";
-import api from "../../configs/api";
-import Error from "../../shared/error/Error";
+import { useState } from "react";
+import useGetProducts from "../../hooks/useGetProducts";
 import Loading from "../../shared/loading/Loading";
+import Error from "../../shared/error/Error";
 import DeleteModal from "./components/DeleteModal";
-import EditModal from "./components/EditModal";
-import AddProductModal from "./components/AddProductModal";
 
 export default function AdminPage() {
+  const { data, isLoading, isError, refetch } = useGetProducts();
   const [menuOpen, setMenuOpen] = useState(false);
-  const [modalOpen, setModalOpen] = useState(false);
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
   const [deleteModal, setDeleteModal] = useState(false);
-  const [editModalOpen, setEditModalOpen] = useState(false);
-  const [editProduct, setEditProduct] = useState(null);
-
-  useEffect(() => {
-    api.get("products")
-      .then((response) => setProducts(response.data))
-      .catch((error) => setError(error))
-      .finally(() => setLoading(false));
-  }, []);
 
   return (
     <div className="relative flex gap-8 container mx-auto font-Quicksand selection:bg-Cultured mb-6">
-      <button onClick={() => setMenuOpen(true)} className="lg:hidden absolute right-10 top-5 cursor-pointer w-9 h-9 flex items-center justify-center p-4
-     bg-gray-200 text-gray-700 rounded-full m-4 z-50">
-        <i className="fa-solid fa-bars"></i>
-      </button>
+      <button onClick={() => setMenuOpen(true)}
+        className="lg:hidden absolute right-10 top-5 cursor-pointer w-9 h-9 flex items-center justify-center p-4 bg-gray-200 text-gray-700 rounded-full m-4 z-50">
+        <i className="fa-solid fa-bars"></i>{" "}
+      </button>{" "}
       {menuOpen && (
-        <div onClick={() => setMenuOpen(false)} className="fixed inset-0 bg-black/30 z-40 lg:hidden"></div>
-      )}
-
-      {/* Sidebar */}
+        <div onClick={() => setMenuOpen(false)} className="fixed inset-0 bg-black/30 z-40 lg:hidden"></div>)}
       <aside className={`fixed lg:static top-0 left-0 h-full w-72 bg-white border-r-2 border-gray-100 flex-col transition-transform duration-500 z-50
           ${menuOpen ? "translate-x-0 flex" : "-translate-x-full hidden"} lg:translate-x-0 lg:flex`}>
         <div className="flex justify-end lg:hidden p-4">
@@ -66,9 +49,7 @@ export default function AdminPage() {
               <span>Orders</span>
             </div>
             <div className="flex items-center space-x-2">
-              <span className="bg-gray-500 text-white text-xs font-bold px-2.5 py-1 rounded-full">
-                12
-              </span>
+              <span className="bg-gray-500 text-white text-xs font-bold px-2.5 py-1 rounded-full">12</span>
               <i className="fa-solid fa-chevron-right text-xs opacity-0 group-hover:opacity-100 transition"></i>
             </div>
           </a>
@@ -98,27 +79,18 @@ export default function AdminPage() {
           </a>
         </nav>
       </aside>
-
-      {/* Main Content */}
       <main className="flex-1 p-4 sm:p-8">
         <div className="flex flex-col lg:flex-row items-start lg:items-center justify-between mb-6">
-          <h2 className="text-gray-700 text-3xl font-bold mb-3">Product Management</h2>
+          <h2 className="text-gray-700 text-3xl font-bold mb-3">
+            Product Management
+          </h2>
           <button onClick={() => setModalOpen(true)} className="px-4 py-2 bg-gray-200 text-gray-600 font-semibold rounded-xl shadow-md hover:shadow-gray-500/50
             transition transform hover:-translate-y-0.5 cursor-pointer text-sm sm:text-base">
             + Add New Product
           </button>
         </div>
 
-        {modalOpen && (
-          <AddProductModal closeModal={() => setModalOpen(false)} />
-        )}
-        {editModalOpen && editProduct && (
-          <EditModal closeModal={() => setEditModalOpen(false)} product={editProduct}/>
-        )}
-
-        {deleteModal && (
-          <DeleteModal closeModal={() => setDeleteModal(false)} />
-        )}
+      {deleteModal && (<DeleteModal closeModal={() => setDeleteModal(false)} />)}
 
         {/* Table */}
         <div className="bg-white rounded-2xl shadow-sm border border-gray-100 overflow-x-auto">
@@ -135,21 +107,21 @@ export default function AdminPage() {
             </thead>
 
             <tbody className="divide-y divide-gray-100">
-              {loading ? (
+              {isLoading ? (
                 <tr>
                   <td colSpan="6" className="text-center py-10">
                     <Loading />
                   </td>
                 </tr>
-              ) : error ? (
+              ) : isError ? (
                 <tr>
                   <td colSpan="6" className="text-center py-10">
                     <Error />
                   </td>
                 </tr>
               ) : (
-                products.map((product) => (
-                  <tr>
+                data.data?.map((product) => (
+                  <tr key={product.id}>
                     <td className="px-4 py-3">
                       <div className="w-14 h-14 rounded-xl overflow-hidden shadow-sm p-1">
                         <img src={product.image} alt={product.title} className="w-full h-full object-contain"/>
@@ -160,26 +132,25 @@ export default function AdminPage() {
                       <p className="line-clamp-1">{product.description}</p>
                     </td>
                     <td className="px-4 py-3 text-center">
-                      <span className="inline-block px-3 py-1 bg-gray-100 text-gray-700 font-bold rounded-full text-xs sm:text-sm">
-                        ${product.price}
-                      </span>
+                      <span className="inline-block px-3 py-1 bg-gray-100 text-gray-700 font-bold rounded-full text-xs sm:text-sm">${product.price}</span>
                     </td>
                     <td className="px-4 py-3">
                       <span className="px-2 py-1 text-xs font-medium">{product.category}</span>
                     </td>
                     <td className="px-4 py-3">
                       <div className="flex items-center justify-center gap-2">
-                        <button onClick={() => { setEditProduct(product); setEditModalOpen(true);}} className="group relative p-2 sm:p-3 bg-Cultured cursor-pointer text-indigo-600 
+                        <button className="group relative p-2 sm:p-3 bg-Cultured cursor-pointer text-indigo-600 
                         rounded-xl hover:shadow-md duration-500 hover:bg-indigo-50 transition-colors">
                           <i className="fa-solid fa-pen-to-square text-sm"></i>
                           <span className="absolute -top-8 left-1/2 -translate-x-1/2 bg-gray-800 text-white text-xs px-2 py-1 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-500">
                             Edit
                           </span>
                         </button>
-                        <button onClick={() => setDeleteModal(true)} className="group relative p-2 sm:p-3 bg-Cultured cursor-pointer text-red-500 
-                        rounded-xl hover:shadow-md duration-500 hover:bg-red-50 transition-colors">
+                        <button onClick={() => { setDeleteModal(true)}}
+                          className="group relative p-2 sm:p-3 bg-Cultured cursor-pointer text-red-500 rounded-xl hover:shadow-md duration-500 hover:bg-red-50">
                           <i className="fa-solid fa-trash text-sm"></i>
-                          <span className="absolute -top-8 left-1/2 -translate-x-1/2 bg-gray-800 text-white text-xs px-2 py-1 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-500 whitespace-nowrap">
+                          <span className="absolute -top-8 left-1/2 -translate-x-1/2 bg-gray-800 text-white text-xs px-2 py-1 rounded-lg 
+                          opacity-0 group-hover:opacity-100 transition-opacity duration-500 whitespace-nowrap">
                             Delete
                           </span>
                         </button>
