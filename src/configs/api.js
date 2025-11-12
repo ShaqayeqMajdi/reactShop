652d286e3552c8f1d282dac3aa4c75f9282ae79d
ShaqayeqMajdi <shaqayeqmajdiazim@gmail.com>
2025-11-12 19:35:35 +0330

    use env var for API base URL

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "https://fakestoreapi.com/",
+// });
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 const api = axios.create({
-  baseURL: "https://fakestoreapi.com/",
+  baseURL: BASE_URL,
 });
 
 export default api;
